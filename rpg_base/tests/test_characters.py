b1565ebbff9c00a042f81af20a37d8c43dd2a3b2
Brent Mitton <bmitton@gatech.edu>
2015-05-06 20:49:09 -0300

    roll tests

--- rpg_base/tests/test_characters.py
+++ rpg_base/tests/test_characters.py
@@ -37,3 +37,54 @@ class CharacterTemplateTestCase(TestCase):
         # no arguments provided to create_character() (besides campaign)
         characters = char_template.create_characters(campaign=self.campaign1)
         self.assertEqual(1, len(characters))
+
+        # 100 characters, because lol
+        characters = char_template.create_characters(campaign=self.campaign1,
+                                                     num=100)
+        self.assertEqual(100, len(characters))
+
+class HitDieTestCase(TestCase):
+
+    def setUp(self):
+        self.char_template = CharacterTemplate(name="TestTemplate")
+    def tearDown(self):
+        pass
+
+    def test_roll(self):
+        """
+        Test that Nolan knows how to generate random numbers
+        """
+        # I don't really know what the best practices are for dealing with
+        # random numbers in tests, but this is mine.
+
+        # Roll a d4
+        d4 = HitDie(die=4)
+
+        rolls = []
+        for i in range(1000):
+            rolls.append(d4.roll())
+
+        self.assertTrue(all(n <= 4 for n in rolls))
+        self.assertTrue(all(n >= 1 for n in rolls))
+
+        # roll a d12+10
+        d12_plus10 = HitDie(die=12, mod=10)
+        rolls = []
+        for i in range(1000):
+            rolls.append(d12_plus10.roll())
+
+        self.assertTrue(all(n <= 22 for n in rolls))
+        self.assertTrue(all(n >= 1 for n in rolls))
+
+        # roll four d8
+        four_d8 = HitDie(die=8, num=4)
+        rolls = []
+        for i in range(1000):
+            rolls.append(four_d8.roll())
+
+        self.assertTrue(all(n <= 32 for n in rolls))
+        self.assertTrue(all(n >= 4 for n in rolls))
+
+
+
+
