468d24e68a8c06f1e5fee543a957f81aac3881ca
ncphillips <ncphillips@upei.ca>
2015-05-11 18:30:01 -0300

    urls file

--- django_rpg/urls.py
+++ django_rpg/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import rpg_base
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(rpg_base.urls))
 ]
