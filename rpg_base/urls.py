4550e8f2bcf31c52d7787f1126a315483a03a10f
ncphillips <ncphillips@upei.ca>
2015-05-11 18:32:14 -0300

    views folder

--- rpg_base/urls.py
+++ rpg_base/urls.py
@@ -1,1 +1,9 @@
-__author__ = 'ncphillips'
+import
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'django_rpg.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include(rpg_base.urls))
+]
