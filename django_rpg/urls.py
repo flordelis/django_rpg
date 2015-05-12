b810b105ddf90a5595e727c63f22e8565fa1eace
ncphillips <ncphillips@upei.ca>
2015-05-12 20:01:23 -0300

    index page now registerd to urls

--- django_rpg/urls.py
+++ django_rpg/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 import rpg_base.urls
+import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'django_rpg.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', views.index, name="index"),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^campaign/^', include(rpg_base.urls)),
+    url(r'^campaign/', include(rpg_base.urls)),
+
 ]
