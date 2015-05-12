21547735734a3c3cebf6a409c0a6cd62761368e2
ncphillips <ncphillips@upei.ca>
2015-05-12 19:58:58 -0300

    rpg_base urls is now included under  in the project url

--- django_rpg/urls.py
+++ django_rpg/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(rpg_base.urls)),
+    url(r'^campaign/^', include(rpg_base.urls)),
 ]
