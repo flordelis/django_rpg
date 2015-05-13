33bfaa718576e54972a5e4396228b686c333738d
ncphillips <ncphillips@upei.ca>
2015-05-12 21:32:24 -0300

    Location view and index are good to go

--- rpg_base/models/location.py
+++ rpg_base/models/location.py
@@ -5,7 +5,7 @@ class Location(models.Model):
     name = models.CharField(max_length=25)
     description = models.CharField(max_length=250)
     parent_location = models.ForeignKey("Location", null=True, blank=True, default=None)
-    campaign = models.ForeignKey("Campaign")
+    campaign = models.ForeignKey("Campaign", null=True, blank=True, default=None)
 
     class Meta:
         app_label = "rpg_base"
