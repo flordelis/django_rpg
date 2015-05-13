17a2e02505e84453ef6ab4f3547fac577baee66c
ncphillips <ncphillips@upei.ca>
2015-05-12 21:29:43 -0300

    Link location to campaign

--- rpg_base/models/location.py
+++ rpg_base/models/location.py
@@ -5,6 +5,7 @@ class Location(models.Model):
     name = models.CharField(max_length=25)
     description = models.CharField(max_length=250)
     parent_location = models.ForeignKey("Location", null=True, blank=True, default=None)
+    campaign = models.ForeignKey("Campaign")
 
     class Meta:
         app_label = "rpg_base"
