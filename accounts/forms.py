13ac7e054a4039ff4aa2fc04f085f95a6a560485
Brent Mitton <bmitton@gatech.edu>
2015-05-12 21:25:38 -0300

    registration works

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, ButtonHolder, Submit
 
@@ -17,3 +18,18 @@ class LoginForm(AuthenticationForm):
             )
         )
 
+
+class RegistrationForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'username',
+            'password1',
+            'password2',
+            ButtonHolder(
+                Submit('register', 'Register', css_class='btn-primary')
+            )
+        )
