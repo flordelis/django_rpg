2aba7bc5d540c86c72b30b9afab171df202b729c
Brent Mitton <bmitton@gatech.edu>
2015-05-19 19:50:26 -0300

    crispier is better on second thought

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,27 @@
-from forms import LoginForm, RegistrationForm, ChangePasswordForm
-from forms import ForgotPasswordForm
+from forms import LoginForm, RegistrationForm
 from django.views import generic
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.views import password_reset
+
+# Create your views here.
+class LoginView(generic.FormView):
+
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(self.request, user)
+            return super(LoginView, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 
 class RegisterView(generic.CreateView):
@@ -33,5 +48,9 @@ class RegisterView(generic.CreateView):
         login(self.request, user)
         return super(RegisterView, self).get_success_url()
 
+class LogoutView(generic.RedirectView):
+    url = reverse_lazy('index')
 
-
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
