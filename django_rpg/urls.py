e500be0f599b18bb79bc6bb25a1fd82cd1959c2b
ncphillips <ncphillips@upei.ca>
2015-06-14 19:43:16 -0300

    API Auth route

--- django_rpg/urls.py
+++ django_rpg/urls.py
@@ -6,11 +6,12 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'django_rpg.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', views.index, name="index"),
-    # url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^$', views.index, name='index'),
+    url(r'^api/', include('rpg_rest_api.urls'), name='api'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^campaigns/(?P<pk>[0-9]+)/$', views.CampaignDetail.as_view()),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^campaign/', include('rpg_base.urls')),
-    url(r'^api/', include('rpg_rest_api.urls'))
 
 ]
