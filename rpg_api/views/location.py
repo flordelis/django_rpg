94067dac9c4faba3401ce0fe3f66bc631790d2e6
ncphillips <ncphillips@upei.ca>
2015-06-14 22:50:54 -0300

    location api filter fields

--- rpg_api/views/location.py
+++ rpg_api/views/location.py
@@ -5,4 +5,7 @@ from rest_framework import viewsets
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+    filter_fields = (
+        'name', 'parent_location__name', 'parent_location',
+    )
 
